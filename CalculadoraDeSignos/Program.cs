3d77eca48147b9f927078696e83fd897b92eb1f2
Lucas pereira de oliveira <46692192+gunn3r71@users.noreply.github.com>
2021-02-28 00:46:26 -0300

    Criando classe signo

--- CalculadoraDeSignos/Program.cs
+++ CalculadoraDeSignos/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using System; //Palavra reservada que adiciona as bibliotecas necessárias para que o programa funcione
 
-namespace CalculadoraDeSignos
+namespace CalculadoraDeSignos //utilizado para organizar seus códigos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Signo signo = new Signo();
+            signo.Nome = "Leão";
+            signo.DiaInicio = 22;
+            signo.MesInicio = 07;
+            signo.DiaFim = 22;
+            signo.MesFim = 08;
+            signo.Descricao = "Carismáticas e terem personalidades fortes";
+
+
+            Console.WriteLine(signo.ToString());
+
         }
     }
 }
---
+++ CalculadoraDeSignos/Signo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraDeSignos
+{
+    class Signo
+    {
+        private string _nome;
+        private int _dia_inicio;
+        private int _mes_inicio;
+        private int _dia_fim;
+        private int _mes_fim;
+        private string _descricao;
+
+        public string Nome
+        {
+            get{ return _nome; }
+            set{ _nome = (value != null && value != "") ? value : "Invalid"; }
+        }
+
+        public int DiaInicio 
+        {
+            get { return _dia_inicio; }
+            set { _dia_inicio = (value >= 1 && value <= 31) ? value : 0; }
+        }   
+
+        public int MesInicio
+        {
+            get { return _mes_inicio; }
+            set { _mes_inicio = (value >= 1 && value <= 12) ? value : 0; }
+        }
+
+        public int DiaFim
+        {
+            get { return _dia_fim; }
+            set { _dia_fim = (value >= 1 && value <= 31) ? value : 0; }
+        }
+
+        public int MesFim
+        {
+            get { return _mes_fim; }
+            set { _mes_fim = (value >= 1 && value <= 12) ? value : 0; }
+        }
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            set { _descricao = (value != null && value != "") ? value : "Invalid"; }
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Olá, sou o signo de {Nome}," +
+                $"\nSou o signo de todas as pessoas que nascem de {DiaInicio}/{MesInicio} à {DiaFim}/{MesFim}." +
+                $"\nPessoas que me tem como signo são conhecidas por serem {Descricao}";
+        }
+    }
+}
